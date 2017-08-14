791dcea619927f7221ee4ddce61270b64380747e
Bill Dodd <billdodd@gmail.com>
2017-08-14 18:17:36 -0500

    added code to try to locate top-level XML tag a few different ways

--- xml_parser_test.py
+++ xml_parser_test.py
@@ -1,6 +1,8 @@
 import argparse
+# import bs4
 from bs4 import BeautifulSoup
 from bs4.diagnose import diagnose
+from bs4.element import Tag
 import requests
 
 default_doc = """
@@ -16,6 +18,8 @@ default_doc = """
 </edmx:Edmx>
 """
 
+# XML parsers: xml, lxml-xml
+# HTML parsers: html.parser, lxml, html5lib
 valid_parsers = ['html.parser', 'lxml', 'lxml-xml', 'xml', 'html5lib']
 
 arg_parser = argparse.ArgumentParser(description='Tool to test various parsers')
@@ -64,11 +68,55 @@ else:
     print('Parsing document with parser "{}"'.format(parser))
     soup = BeautifulSoup(doc, parser)
 
+# print the parsed document
 print()
 print('Parsed document:')
 print()
 print(soup.prettify())
 
+# is the document XML?
+print()
+print('is_xml = {}'.format(soup.is_xml))
 
+# try to get the expected Redfish schema root tag 'Edmx' (or 'edmx:edmx' if using HTML parser)
+print()
+if soup.is_xml:
+    tag = soup.Edmx
+    if tag is not None:
+        print('Found tag named "Edmx"')
+        print('tag name = {}'.format(tag.name))
+        print('tag namespace = {}'.format(tag.namespace))
+        print('tag prefix = {}'.format(tag.prefix))
+    else:
+        print('Did not find tag named "Edmx"')
+else:
+    tag = soup.find('edmx:edmx')
+    if tag is not None:
+        print('Found tag named "edmx:edmx"')
+        print('tag name = {}'.format(tag.name))
+        print('tag namespace = {}'.format(tag.namespace))
+        print('tag prefix = {}'.format(tag.prefix))
+    else:
+        print('Did not find tag named "edmx:edmx"')
+        html = soup.html
+        if html is not None:
+            body = html.body
+            if body is not None:
+                if len(body.contents) > 0:
+                    if isinstance(body.contents[0], Tag):
+                        print('Found tag named "{}"'.format(body.contents[0].name))
+                        print('len(html.body.contents) = {}'.format(len(body.contents)))
+                        print('html.body.contents[0].name = {}'.format(body.contents[0].name))
+                        print('html.body.contents[0].namespace = {}'.format(body.contents[0].namespace))
+                        print('html.body.contents[0].prefix = {}'.format(body.contents[0].prefix))
 
-
+# look for any tags in the first 10 children of the document
+print()
+print('len(soup.contents) = {}'.format(len(soup.contents)))
+for n in range(10):
+    if len(soup.contents) > n:
+        print('child element [{}] found, type = {}'.format(n, type(soup.contents[n])))
+        if isinstance(soup.contents[n], Tag):
+            print('    soup.contents[{}].name = {}'.format(n, soup.contents[n].name))
+            print('    soup.contents[{}].namespace = {}'.format(n, soup.contents[n].namespace))
+            print('    soup.contents[{}].prefix = {}'.format(n, soup.contents[n].prefix))
